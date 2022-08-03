251b835d739565af84a4ab1112c2272192b12538
Vihar <vihar1998x@gmail.com>
2022-08-03 11:49:07 +0530

    + Responsive design for profile

--- src/components/TopTracks/TopTracks.jsx
+++ src/components/TopTracks/TopTracks.jsx
@@ -7,6 +7,7 @@ import { useEffect } from "react"
 
 export default function TopTracks() {
     const dispatch = useDispatch()
+    
     const { topTracks, currentFilter, isLoading } = useSelector(state => state.topTracks)
 
     const filters = [
