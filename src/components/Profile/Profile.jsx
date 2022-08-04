96db73be4251d531ec7d3ff6886eb18d7e0afbf5
Vihar <vihar1998x@gmail.com>
2022-08-05 00:24:48 +0530

    - Remove console logs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -28,9 +28,6 @@ export default function Profile() {
         }
     }, [])
 
-    console.log("artists",userTopArtists)
-    console.log("tracks" ,userTopTracks)
-
     return <div className="profile">
         {
             isLoading
